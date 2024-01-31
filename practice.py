bda2002e53802d263e1019505de7a8374899033e
AleIsPale435 <alebarr564@gmail.com>
2024-01-30 22:16:57 -0700

    Create practice.py

---
+++ practice.py
@@ -0,0 +1,8 @@
+#Version practice for CS3300
+print("This is a test: \n")
+x_value = 5
+print("This is x: %d" % x_value)
+y_value = 10
+print("This is y: %d" % y_value)
+z_value = x_value * y_value
+print("The answer to x * y is : %d" % z_value)
